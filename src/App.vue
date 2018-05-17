b5bcf30bee641a575c7ceb1e65e350bc00c573e3
nakanokurenai <master@yukarium.net>
2018-05-17 15:24:19 +0900

    適当にやってみようと

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <p>Hello!</p>
+  <div>
+    <li>
+      <ul v-for="post in posts" :key="post.id">
+        <section>
+          <h1>{{post.title}}</h1>
+          <p>{{post.body}}</p>
+        </section>
+      </ul>
+    </li>
+  </div>
 </template>
 
 <script>
+export default {
+  data: () => ({
+    posts: [
+      {
+        'id': 1,
+        'title': 'ほげ',
+        'body': 'ふが'
+      },
+      {
+        id: 2,
+        title: 'hoge',
+        body: 'huga'
+      }
+    ]
+  })
+}
 </script>
 
 <style scoped>
