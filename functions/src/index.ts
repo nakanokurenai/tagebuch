7ea269699bb36ac017c448a235c12ab039a11f33
nakanokurenai <master@yukarium.net>
2018-05-30 12:05:06 +0900

    setup functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,34 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as URL from 'url'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+const firestore = admin.firestore()
+const articlesCollection = firestore.collection('articles')
+
+export const articles = functions.https.onRequest(async (req, res) => {
+  const articlesSnapshot = await articlesCollection.orderBy('updated_at', 'desc').get()
+  const articlesData: FirebaseFirestore.DocumentData[] = []
+  articlesSnapshot.forEach(document => articlesData.push(
+    Object.assign({
+      id: document.id
+    }, document.data())
+  ))
+  return res.send({
+    articles: articlesData,
+    length: articlesData.length
+  })
+})
+
+export const article = functions.https.onRequest(async (req,res) => {
+  const url = URL.parse(req.url)
+  if (!url.pathname || !url.pathname.startsWith('/api/articles/')) {
+    return res.status(500).send('BAAD')
+  }
+  const articleID = url.pathname.split('/api/articles/')[1]
+  const articleDocument = await articlesCollection.doc(articleID).get()
+  if (!articleDocument.exists) return res.status(404).send('ENOENT')
+  return res.send({
+    article: articleDocument.data()
+  })
+})
