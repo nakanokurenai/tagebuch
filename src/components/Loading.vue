b8762eb43123c85ae91f31c8f8c99362366f082e
nakanokurenai <master@yukarium.net>
2018-06-26 15:15:58 +0900

    Loading component will override all content on page

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="loading sk-wave">
-    <div class="sk-rect sk-rect1"></div>
-    <div class="sk-rect sk-rect2"></div>
-    <div class="sk-rect sk-rect3"></div>
-    <div class="sk-rect sk-rect4"></div>
-    <div class="sk-rect sk-rect5"></div>
+  <div class="container">
+    <div class="loading sk-wave">
+      <div class="sk-rect sk-rect1"></div>
+      <div class="sk-rect sk-rect2"></div>
+      <div class="sk-rect sk-rect3"></div>
+      <div class="sk-rect sk-rect4"></div>
+      <div class="sk-rect sk-rect5"></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
   @import url(~spinkit/css/spinners/3-wave.css);
+
+  .container {
+    position: absolute;
+    margin: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: #fff;
+    z-index: 2147483647; /* max priority */
+  }
 
   .loading {
     position: absolute;
