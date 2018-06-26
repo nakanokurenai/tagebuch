5c6a12cd477e716cfbc74f95701c7a767dcc3aea
nakanokurenai <master@yukarium.net>
2018-06-26 11:24:27 +0900

    wraps spinkit in Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loading sk-wave">
+    <div class="sk-rect sk-rect1"></div>
+    <div class="sk-rect sk-rect2"></div>
+    <div class="sk-rect sk-rect3"></div>
+    <div class="sk-rect sk-rect4"></div>
+    <div class="sk-rect sk-rect5"></div>
+  </div>
+</template>
+
+<style scoped>
+  @import url(~spinkit/css/spinners/3-wave.css);
+
+  .loading {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+  }
+  /* disable lit */
+  .loading * {
+    margin: 0;
+    box-sizing: content-box;
+  }
+</style>
+
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="init sk-wave" v-if="onInitializing">
-    <div class="sk-rect sk-rect1"></div>
-    <div class="sk-rect sk-rect2"></div>
-    <div class="sk-rect sk-rect3"></div>
-    <div class="sk-rect sk-rect4"></div>
-    <div class="sk-rect sk-rect5"></div>
-  </div>
+  <Loading v-if="onInitializing" />
   <div class="c" v-else>
     <h1>Das Tagebuch</h1>
     <div id=list>
@@ -22,11 +16,10 @@
 import firebase from '@firebase/app'
 import Raven from 'raven-js'
 
-import 'spinkit/css/spinners/3-wave.css'
-
 import { articlesCollection } from '~/firebase'
 
 import PostPreview from '~/components/PostPreview.vue'
+import Loading from '~/components/Loading.vue'
 
 export default {
   data: () => ({
@@ -38,28 +31,14 @@ export default {
     this.onInitializing = false
   },
   components: {
-    PostPreview
+    PostPreview,
+    Loading
   }
 }
 </script>
 
 <style scoped>
-.init {
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-}
-/* disable lit */
-.init * {
-  margin: 0;
-  box-sizing: content-box;
-}
-
 ol {
   list-style: none;
 }
 </style>
-
